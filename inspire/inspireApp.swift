b1f45d784f7b9d4b7c39aa9d726e46459ca38d6e
highthem <highthem@icloud.com>
2020-10-30 09:38:49 +0400

    Initial Commit

---
+++ inspire/inspireApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspireApp.swift
+//  inspire
+//
+//  Created by Highthem on 30/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct inspireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
